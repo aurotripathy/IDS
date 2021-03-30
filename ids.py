8271ef0721fdf6c4f764ab48f2d77f71d7ba0455
Auro Tripathy <ipserv@yahoo.com>
2021-03-29 21:34:36 -0700

    var name change

--- ids.py
+++ ids.py
@@ -42,21 +42,21 @@ print('test cols', all_test.columns)
 all_data = all_data.append(all_test, ignore_index=True)
 print('total train + test records', len(all_data))
 
-col_grp_1 = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land',
+column_grp_1 = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land',
              'wrong_fragment', 'urgent']
 
-col_grp_2 = ['hot', 'num_failed_logins', 'logged_in', 'num_compromised', 'root_shell', 'su_attempted',
+column_grp_2 = ['hot', 'num_failed_logins', 'logged_in', 'num_compromised', 'root_shell', 'su_attempted',
              'num_root', 'num_file_creations', 'num_shells', 'num_access_files', 'num_outbound_cmds',
              'is_host_login', 'is_guest_login']
 
-col_grp_3 = ['count', 'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',
+column_grp_3 = ['count', 'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',
              'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate']
 
-col_grp_4 = ['dst_host_count', 'dst_host_srv_count', 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',
+column_grp_4 = ['dst_host_count', 'dst_host_srv_count', 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',
              'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',
              'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate']
 
-columns = col_grp_1 + col_grp_2 + col_grp_3 + col_grp_4
+columns = column_grp_1 + column_grp_2 + column_grp_3 + column_grp_4
 all_data.columns = columns + ['class', 'successful_pred']
 
 print(len(columns))
